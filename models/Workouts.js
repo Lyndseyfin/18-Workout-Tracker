ab5984df71cd6543d3502756c2d70700febc6ff3
Lyndsey Finamore <lyndseyfinamore@gmail.com>
2021-06-21 14:54:07 -0600

    routes/models

---
+++ models/Workouts.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now
+  },
+  exercises: [
+    {
+      name: {
+        type: String,
+        trim: true,
+        required: "Enter exrcise name."
+      },
+      type: {
+        type: String,
+        trim: true,
+        required: "Enter type of exercise (ie. cardio, cycling)."
+      },
+      weight: {
+        type: Number,
+        default: 0
+      },
+      sets: {
+        type: Number,
+        default: 0
+      },
+      reps: {
+        type: Number,
+        default: 0
+      },
+      duration: {
+        type: Number,
+        default: 0,
+        required: "Duration is required."
+      },
+      distance: {
+        type: Number,
+        default: 0
+      }
+    }
+  ],
+});
+
+const Workout = mongoose.model("/Workouts", workoutSchema);
+
+module.exports = Workout;
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,12 @@
 const router = require("express").Router();
-const Workout = require("../models/workout.js");
+const Workout = require("../models/Workouts");
 
 
 router.get("/api/workouts", (req, res) => {
   Workout.find({})
     // .sort({ date: -1 })
-    .then(dbWorkouts => {
-      res.json(dbWorkouts);
+    .then(dbWorkout => {
+      res.json(dbWorkout);
     })
     .catch(err => {
       res.status(400).json(err);
@@ -26,23 +26,31 @@ router.get("/api/workouts/range", (req, res) => {
 router.post("/api/workouts", (req, res) => {
   console.log(Workout);
 
-  Workout.create(req.body)
-    .then(dbWorkouts => {
-      res.json(dbWorkouts);
+  Workout.create({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
     })
     .catch(err => {
       res.status(400).json(err);
     });
 });
 
-router.post("/api/workout/bulk", ({ body }, res) => {
-  Workout.insertMany(body)
-    .then(dbWorkouts => {
-      res.json(dbWorkouts);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
+// router.post("/api/workout/bulk", ({ body }, res) => {
+//   Workout.insertMany(body)
+//     .then(dbWorkouts => {
+//       res.json(dbWorkouts);
+//     })
+//     .catch(err => {
+//       res.status(400).json(err);
+//     });
+// });
+router.put("/api/workouts/:id", (req, res)=>{
+  workout.findByIdAndUpdate(
+      req.params.id,
+      {$push : {exercises: req.body}}
+  )
+  .then( results=> res.json(results))
+  .catch(err=> res.json(err))
 });
 
 
