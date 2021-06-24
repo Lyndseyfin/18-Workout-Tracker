d44e1b253029306f0a5932d7503dc324229bd7a6
Lyndsey Finamore <lyndseyfinamore@gmail.com>
2021-06-24 12:38:49 -0600

    heroku push/final

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/blooming-stream", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/safe-ridge", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
